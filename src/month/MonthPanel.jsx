4446d832fe2e201daddb0e8531746021af9bb4d9
Arulkumar <arulkumars@visualbi.com>
2019-03-22 14:42:38 +0530

    period selector range slider

--- src/month/MonthPanel.jsx
+++ src/month/MonthPanel.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import 'react-input-range/lib/css/index.css'
+import InputRange from 'react-input-range';
 import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
 import MonthTable from './MonthTable';
-
 function goYear(direction) {
   const next = this.state.value.clone();
   next.add(direction, 'year');
@@ -14,6 +16,12 @@ function onInputChange(event) {
     this.props.calendarprops[event.target.name] = event.target.value;
     this.props.onRangeSelect();
   }
+  
+}
+function onRangeChange(value) {   
+    this.props.calendarprops["rangestart"] = this.state.valuerange.min;
+    this.props.calendarprops["rangeend"] = this.state.valuerange.max;
+    this.props.onRangeSelect(); 
   
 }
 function noop() {
@@ -42,16 +50,35 @@ const MonthPanel = createReactClass({
     this.nextYear = goYear.bind(this, 1);
     this.previousYear = goYear.bind(this, -1);
     this.onchange =  onInputChange.bind(this);
+    this.onrangechange =  onRangeChange.bind(this);
     this.prefixCls = `${props.rootPrefixCls}-month-panel`;
     return {
-      value: props.value || props.defaultValue,
+      value:  props.value || props.defaultValue,
+      minrange:props.calendarprops.minrange,
+      maxrange:props.calendarprops.maxrange,
+      valuerange: {
+        min: props.calendarprops.rangestart,
+        max: props.calendarprops.rangeend,
+      }
     };
+  
+  
   },
 
   componentWillReceiveProps(nextProps) {
     if ('value' in nextProps) {
       this.setState({
         value: nextProps.value,
+      });
+    }
+    if ('calendarprops' in nextProps) {
+      this.setState({
+        minrange:nextProps.calendarprops.minrange,
+        maxrange:nextProps.calendarprops.maxrange,
+        valuerange: {
+          min: nextProps.calendarprops.rangestart,
+          max: nextProps.calendarprops.rangeend,
+        }
       });
     }
   }, 
@@ -87,16 +114,26 @@ const MonthPanel = createReactClass({
    };
    const inputStyle = {
 height:'30px'
+
    };
 
     return (
       <div className={prefixCls} style={props.style}>
-        <div>
+        <div>       
           <div className={`${prefixCls}-header`}>
-            {showIf(this.props.calendarprops.rangemode,
+              {showIf(this.props.calendarprops.rangemode && this.props.calendarprops.rangetype == "slider",
+              <InputRange           
+              maxValue={this.state.maxrange}
+              minValue={this.state.minrange}
+              formatLabel={value => `${value}`}
+              value={this.state.valuerange}
+              onChange={value => this.setState({ valuerange: value })}
+              onChangeComplete={this.onrangechange} />
+            )}
+            {showIf(this.props.calendarprops.rangemode && this.props.calendarprops.rangetype !== "slider",
               <span style={headfontStyle}>From </span>
             )}
-            {showIf(this.props.calendarprops.rangemode,
+            {showIf(this.props.calendarprops.rangemode && this.props.calendarprops.rangetype !== "slider",
               <input
                 style={inputStyle}
                 className={`${prefixCls}-start-year-input`}
@@ -105,10 +142,10 @@ height:'30px'
                 value={this.props.calendarprops.rangestart}
                 onChange={this.onchange}
               />)}
-            {showIf(this.props.calendarprops.rangemode,
+            {showIf(this.props.calendarprops.rangemode && this.props.calendarprops.rangetype !== "slider",
               <span style={headfontStyle}>To </span>
             )}
-            {showIf(this.props.calendarprops.rangemode,
+            {showIf(this.props.calendarprops.rangemode && this.props.calendarprops.rangetype !== "slider",
               <input
                 style={inputStyle}
                 className={`${prefixCls}-end-year-input`}
